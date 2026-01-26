e8bda2fbfddee374207135ef87a08ac912948100
Saurav4359 <isauravsharmaokay4359@gmail.com>
2026-01-27 05:19:03 +0530

    file st

--- frontend/pages/Editor.tsx
+++ frontend/pages/Editor.tsx
@@ -1,19 +1,31 @@
 import Editor from "@monaco-editor/react";
-export function CodeEditor({ language }: { language: string }) {
+export function CodeEditor({
+  language,
+}: {
+  language: string;
+    
+}) {
+ 
+ const path ={ 
+    javascript : "// write javascript",
+    java : "// write java code",
+    cpp :  "// C++ code"
+  }
   return (
     <Editor
+      key={language}
       className="bg-black "
       height="600px"
       width="600px"
       defaultLanguage={language}
-      defaultValue="// Some Path"
-      theme="vs-dark" 
+      defaultValue={path[language as keyof typeof path]}
+      theme="vs-dark"
       options={{
-        cursorBlinking:'blink',
+        cursorBlinking: "blink",
         formatOnPaste: true,
         minimap: {
-            enabled:false
-        }
+          enabled: false,
+        },
       }}
     />
   );
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,27 @@ import { useRef, useState } from "react";
 
 function App() {
   const lanRef = useRef<HTMLSelectElement>(null);
-  const [value, setValue] = useState("cpp");
-
+  const [language, setlanguage] = useState("cpp");
+   
+ 
   function select() {
     if (!lanRef.current) return;
-    setValue(lanRef.current.value);
+    setlanguage(lanRef.current?.value);
   }
-
+  
+ 
   return (
     <div className="h-screen bg-black/50 flex justify-center items-center">
       <div className="grid justify-center">
         <div className="h-10 w-150 bg-amber-300">
-          <select
-            ref={lanRef}
-            onChange={select}
-            defaultValue="cpp"
-          >
+          <select ref={lanRef} onChange={select} defaultValue="cpp">
             <option value="javascript">Javascript</option>
             <option value="java">Java</option>
             <option value="cpp">C++</option>
           </select>
         </div>
 
-        <CodeEditor language={value} />
+        <CodeEditor language={language} />
       </div>
     </div>
   );
