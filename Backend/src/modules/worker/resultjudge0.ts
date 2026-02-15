fd4c28c726065575e27722a0660075670471b0e3
Saurav4359 <isauravsharmaokay4359@gmail.com>
2026-02-16 01:26:39 +0530

    error-handling-done

--- Backend/src/modules/worker/resultjudge0.ts
+++ Backend/src/modules/worker/resultjudge0.ts
@@ -4,10 +4,11 @@ export async function Result(token: string) {
   let result;
   for (let i = 0; i < 30; i++) {
     result = await getResult(token);
-
-    if (result.status.id != 1 && result.status.id != 2) {
-      return result;
-    }
+    const id = result.status.id;
+    if (id === 3) return result;
+    else if (id === 1 && id === 2) continue;
+    else if (id === 5) return 5;
+    else if (id === 6) return 6;
     await new Promise((res) => setTimeout(res, 200));
   }
   return -1;
--- Backend/src/modules/worker/worker.ts
+++ Backend/src/modules/worker/worker.ts
@@ -6,100 +6,89 @@ import { setVerdict } from "../verdict/verdict";
 import { Result } from "./resultjudge0";
 import { normalize } from "../../utils/normalise";
 
-// const worker = new Worker(
-//   "Code",
-//   async (job) => {
-//     try {
-//       let result;
-//       let testcasePassed = 1;
-//       const data = job.data;
-//       console.log(data);
-//       const HiddenTestCase = await GetHiddenTest(data.problemId);
-//       const VisibleTestCase = await GetVisibleTest(data.problemId);
+const worker = new Worker(
+  "Code",
+  async (job) => {
+    try {
+      let result;
+      let testcasePassed = 1;
+      const data = job.data;
+      const HiddenTestCase = await GetHiddenTest(data.problemId);
+      const VisibleTestCase = await GetVisibleTest(data.problemId);
 
-//       for (const test of VisibleTestCase) {
-//         const input = await DownloadFile(test.inputPath);
-//         const output = await DownloadFile(test.outputPath);
-//         data.stdin = input;
-//         console.log(data);
-//         const token = await submitCode(data);
-//         result = await Result(token);
-//         if (result === -1) {
-//           return await setVerdict(data, "FAILED");
-//         }
+      for (const test of VisibleTestCase) {
+        const input = await DownloadFile(test.inputPath);
+        const output = await DownloadFile(test.outputPath);
+        data.stdin = input;
 
-//         console.log(output);
-//         console.log(result.stdout.trim());
-//         if (normalize(output as string) === normalize(result.stdout.trim())) {
-//           testcasePassed++;
-//         } else {
-//           console.log("testCase failed At ", testcasePassed);
-//           await setVerdict(data, "FAILED");
-//           return `testCase failed At ${testcasePassed}`;
-//         }
-//         if (result.time > data.time) {
-//           await setVerdict(data, "TLE");
-//           return;
-//         } else if (result.memory > data.memory * 1024) {
-//           await setVerdict(data, "MLE");
-//           return;
-//         }
-//       }
-//       for (const test of HiddenTestCase) {
-//         const input = await DownloadFile(test.inputPath);
-//         const output = await DownloadFile(test.outputPath);
-//         data.stdin = input;
-//         const token = await submitCode(data);
-//         result = await Result(token);
-//         if (result === -1) {
-//           return await setVerdict(data, "FAILED");
-//         }
-//         console.log(JSON.stringify(output));
-//         console.log(JSON.stringify(result.stdout));
+        const token = await submitCode(data); //getting token after submitting the code ...
+        result = await Result(token);
+        if (result === -1 || result === 6)
+          return await setVerdict(data, "FAILED");
+        else if (result === 5) return await setVerdict(data, "TLE");
 
-//         if (normalize(output as string) === normalize(result.stdout.trim())) {
-//           testcasePassed++;
-//         } else {
-//           console.log("testCase failed At ", testcasePassed);
-//           await setVerdict(data, "FAILED");
-//           return `testCase failed At ${testcasePassed}`;
-//         }
+        // console.log(output);
+        // console.log(result?.stdout.trim());
+        if (normalize(output as string) === normalize(result?.stdout?.trim())) {
+          testcasePassed++;
+        } else {
+          console.log("testCase failed At ", testcasePassed);
+          await setVerdict(data, "FAILED");
+          return `testCase failed At ${testcasePassed}`;
+        }
+        if (result.time > data.time) {
+          await setVerdict(data, "TLE");
+          return;
+        } else if (result.memory > data.memory * 1024) {
+          await setVerdict(data, "MLE");
+          return;
+        }
+      }
+      for (const test of HiddenTestCase) {
+        const input = await DownloadFile(test.inputPath);
+        const output = await DownloadFile(test.outputPath);
+        data.stdin = input;
+        const token = await submitCode(data);
+        result = await Result(token);
+        if (result === -1 || result === 6)
+          return await setVerdict(data, "FAILED");
+        else if (result === 5) return await setVerdict(data, "TLE");
+        // console.log(JSON.stringify(output));
+        // console.log(JSON.stringify(result.stdout));
 
-//         if (result.time > data.time) {
-//           return await setVerdict(data, "TLE");
-//         } else if (result.memory > data.memory * 1024) {
-//           return await setVerdict(data, "MLE");
-//         }
-//       }
-//       if (result.status.description === "Accepted") {
-//         await setVerdict(data, "AC");
-//       } else {
-//         await setVerdict(data, "FAILED");
-//       }
+        if (normalize(output as string) === normalize(result?.stdout?.trim())) {
+          testcasePassed++;
+        } else {
+          console.log("testCase failed At ", testcasePassed);
+          await setVerdict(data, "FAILED");
+          return `testCase failed At ${testcasePassed}`;
+        }
 
-//       return console.log("TestCase passed : ", testcasePassed);
-//     } catch (e) {
-//       console.log("Server Crashed: ", e);
-//       return;
-//     }
-//   },
-//   {
-//     connection: {
-//       host: "localhost",
-//       port: 6379,
-//     },
-//   },
-// );
+        if (result.time > data.time) {
+          return await setVerdict(data, "TLE");
+        } else if (result.memory > data.memory * 1024) {
+          return await setVerdict(data, "MLE");
+        }
+      }
+      if (result.status.description === "Accepted") {
+        await setVerdict(data, "AC");
+      } else {
+        await setVerdict(data, "FAILED");
+      }
 
-const worker= new Worker("Code", async (job) => {
-   console.log("Job done " , job.data);
-},{
-  connection : {
-    host: "localhost",
-    port : 6379
-  
-  }
-})
+      return console.log("TestCase passed : ", testcasePassed);
+    } catch (e) {
+      console.log("Server Crashed: ", e);
+      return;
+    }
+  },
+  {
+    connection: {
+      host: "localhost",
+      port: 6379,
+    },
+  },
+);
 
 worker.on("failed", (job, err) => {
   console.log("Error in job ", err);
